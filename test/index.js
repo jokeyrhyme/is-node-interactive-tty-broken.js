3f1cefe27069a1062fa6dc190b0d76af4b49ee5b
Ron Waldon <jokeyrhyme@gmail.com>
2016-06-02 11:14:48 +1000

    add non-Windows tests

--- test/index.js
+++ test/index.js
@@ -10,8 +10,14 @@ assert.equal(typeof isFixed(), 'boolean')
 assert.notEqual(isBroken(), isFixed())
 
 // force Windows tests
+// https://github.com/nodejs/node/issues/5384
 
 assert.equal(typeof isBroken(null, 'win32'), 'boolean')
 assert.equal(typeof isFixed(null, 'win32'), 'boolean')
 
 assert.notEqual(isBroken(null, 'win32'), isFixed(null, 'win32'))
+
+// force non-Window tests
+
+assert.equal(isBroken(null, 'darwin'), false)
+assert.equal(isFixed(null, 'darwin'), true)
